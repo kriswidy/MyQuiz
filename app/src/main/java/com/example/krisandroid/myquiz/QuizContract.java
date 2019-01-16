dae29a415735e3288d957cc0437ae5b33134c8e6
Kris Widy. Febyanti <kwf2102@gmail.com>
2019-01-16 16:27:27 +0700

    highscore failed

---
+++ app/src/main/java/com/example/krisandroid/myquiz/QuizContract.java
@@ -0,0 +1,30 @@
+package com.example.krisandroid.myquiz;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by kris on 16/01/19.
+ */
+
+public final class QuizContract {
+
+    private  QuizContract() {
+
+    }
+
+    public static class CategoriesTable implements BaseColumns{
+        public static final String TABLE_NAME = "quiz_categories";
+        public static final String COLUMN_NAME = "name";
+    }
+
+    public  static class  QuestionsTable implements BaseColumns{
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTION = "question";
+        public static final String COLUMN_OPTION1 = "option1";
+        public static final String COLUMN_OPTION2 = "option2";
+        public static final String COLUMN_OPTION3 = "option3";
+        public static final String COLUMN_ANSWER_NR = "answer_nr";
+        public static final String COLUMN_DIFFICULTY = "difficulty";
+        public static final String COLUMN_CATEGORY_ID = "category_id";
+    }
+}
